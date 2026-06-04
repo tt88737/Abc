a04bbe250d88d63ba2926fb53ca55fef11170cb6
tt88737 <tt8873756@gmail.com>
2026-06-04 16:31:04 +0800

    chore: refresh generated data

--- data/three-compound-state.js
+++ data/three-compound-state.js
@@ -1,11 +1,11 @@
 window.__THREE_COMPOUND_STATE__ = {
-  "generatedAt": "2026-06-04 16:24:21",
+  "generatedAt": "2026-06-04 16:27:58",
   "items": [
     {
       "source": "am",
       "year": "2026",
       "latestIssue": 154,
-      "computedAt": "2026-06-04 16:24:21",
+      "computedAt": "2026-06-04 16:27:58",
       "status": "cached",
       "pools": [
         {
@@ -9766,7 +9766,7 @@ window.__THREE_COMPOUND_STATE__ = {
       "source": "hk",
       "year": "2026",
       "latestIssue": 59,
-      "computedAt": "2026-06-04 16:24:21",
+      "computedAt": "2026-06-04 16:27:58",
       "status": "cached",
       "pools": [
         {
